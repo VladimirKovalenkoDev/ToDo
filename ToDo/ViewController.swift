fa5ad3ce50f49328d6b73e1aec85e77878874f8d
Vladimir Kovalenko <vovkazon@mail.ru>
2020-06-24 20:36:05 +0300

    Initial Commit

---
+++ ToDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Владимир Коваленко on 24.06.2020.
+//  Copyright © 2020 Vladimir Kovalenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
